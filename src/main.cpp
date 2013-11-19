1f28b4c196445b3e592b8e7fa974e8e47e2684a9
TheDespite <despite.mn@gmail.com>
2013-11-20 00:07:23 +0200

    Behold: The ballgame

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,31 @@
 
 using namespace std;
 
-GLuint _textureId;
+GLuint _weedTexId;
+GLuint _ballTexId;
+
+GLfloat pX, pY;
+GLfloat pvX, pvY;
+
+bool keyDown = false;
 
 void handleKeypress(unsigned char key, int x, int y)
 {
 	switch (key)
 	{
+	case 0x20:
+		pvY += (pvY == 0) ? 0.5f : 0.0f;
+		break;
 	case 27: // Escape
 		exit(0);
+	case 'a':
+		pvX -= (pvX > -0.1f) ? 0.05f : 0.0f;
+		keyDown = true;
+		break;
+	case 'd':
+		pvX += (pvX < 0.1f) ? 0.05f : 0.0f;
+		keyDown = true;
+		break;
 	default:
 		break;
 	}
@@ -25,7 +42,9 @@ GLuint loadTexture(const char* path)
 	strcat_s(fullPath, path);
 
 	FIBITMAP *bitmap = FreeImage_Load(FIF_PNG, fullPath, 0);
-	BYTE* bitmapBytes = FreeImage_GetBits(bitmap);
+	bitmap = FreeImage_ConvertTo24Bits(bitmap);
+	BYTE* bitmapBits = FreeImage_GetBits(bitmap);
+
 
 	GLuint textureId;
 	glGenTextures(1, &textureId);
@@ -38,8 +57,8 @@ GLuint loadTexture(const char* path)
 				 64,
 				 0,
 				 GL_RGB,
-				 GL_BYTE,
-				 bitmapBytes);
+				 GL_UNSIGNED_BYTE,
+				 bitmapBits);
 
 	return textureId;
 
@@ -49,12 +68,18 @@ GLuint loadTexture(const char* path)
 void initRendering()
 {
 	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
-	glEnable(GL_NORMALIZE);
 	glEnable(GL_COLOR_MATERIAL);
 
-	_textureId = loadTexture("weed.png");
+	_weedTexId = loadTexture("weed.png");
+	_ballTexId = loadTexture("bounce.png");
+
+	pX = 0.0f;
+	pY = 1.0f;
+
+	pvX = 0.0f;
+	pvY = 0.0f;
+
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 }
 
 void handleResize(int w, int h)
@@ -72,33 +97,83 @@ void drawScene()
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-	glTranslatef(0.0f, 0.0f, -5.0f);
+	glTranslatef(-5.0f, -2.1f, -15.0f);
 
 	glEnable(GL_TEXTURE_2D);
-	glBindTexture(GL_TEXTURE_2D, _textureId);
+	glBindTexture(GL_TEXTURE_2D, _weedTexId);
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-	glColor3f(1.0f, 0.2f, 0.2f);
+	glColor3f(1.0f, 1.0f, 1.0f);
 	glBegin(GL_QUADS);
 
-	glNormal3f(0.0, 0.0f, 1.0f);
 	glTexCoord2f(0.0f, 0.0f);
-	glVertex3f(-0.5f, -0.5f, 0.0f);
+	glVertex3f(10.0f, 0.0f, 0.0f);
 
-	glTexCoord2f(1.0f, 0.0f);
-	glVertex3f(-0.5f, 0.5f, 0.0f);
+	glTexCoord2f(10.0f, 0.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f);
 
-	glTexCoord2f(1.0f, 1.0f);
-	glVertex3f(0.5f, 0.5f, 0.0f);
+	glTexCoord2f(10.0f, 1.0f);
+	glVertex3f(0.0f, 1.0f, 0.0f);
 
 	glTexCoord2f(0.0f, 1.0f);
-	glVertex3f(0.5f, -0.5f, 0.0f);
+	glVertex3f(10.0f, 1.0f, 0.0f);
+
+	glEnd();
+
+
+	// Draw player
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, _ballTexId);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	glTranslatef(pX, pY, 0.0f);
+
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glBegin(GL_QUADS);
+
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3f(1.0f, 0.0f, 0.0f);
+
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+
+	glTexCoord2f(1.0f, 1.0f);
+	glVertex3f(0.0f, 1.0f, 0.0f);
+
+	glTexCoord2f(0.0f, 1.0f);
+	glVertex3f(1.0f, 1.0f, 0.0f);
 
 	glEnd();
 
 	glutSwapBuffers();
+}
+
+void Update(int i)
+{
+	if (!keyDown)
+		pvX += (pvX > 0) ? -0.005f : 0.005f;
+
+	if (pvY != 0.0f || pY != 1.0f)
+		pvY -= 0.05f;
+
+	keyDown = false;
+
+	pX += pvX;
+
+	if (pY + pvY < 1.0f)
+	{
+		pY = 1.0f;
+		pvY = 0.0f;
+	}
+	else
+		pY += pvY;
+
+	drawScene();
+	glutTimerFunc(1000 / 60, Update, 0);
 }
 
 int main(int argc, char* argv[])
@@ -113,6 +188,8 @@ int main(int argc, char* argv[])
 	glutDisplayFunc(drawScene);
 	glutKeyboardFunc(handleKeypress);
 	glutReshapeFunc(handleResize);
+
+	glutTimerFunc(1000 / 60, Update, 0);
 
 	glutMainLoop();
 	return 0;
